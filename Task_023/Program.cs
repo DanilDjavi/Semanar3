4c87dd0ad2efb0227ce81a98bb34be7127b417ef
DJAVI <badamshindanil@yandex.ru>
2023-02-09 21:23:20 +0500

    Add task 23

---
+++ Task_023/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+String Str = ": ";
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+    for(int i=1; i != number+1; i++)
+    {
+        Str=Str+ Math.Pow(i, 3) + "; ";
+    }
+}
+else Console.WriteLine("Ошибка");
+Console.Write($"Ряд кубов чисел от 1 до {number}");
+Console.WriteLine(Str);
